a6e0375bf0006ed9606c7853f8cc9ca6aadb2e8c
pavlo <pavlo.pohrebnyi@asg.com>
2019-10-11 20:03:58 +0300

    Example of shared lib call (28)

--- vars/dataflow.groovy
+++ vars/dataflow.groovy
@@ -1,9 +1,15 @@
+@Grapes([
+    @Grab(group='com.google.apis', module='google-api-services-dataflow', version='v1b3-rev266-1.25.0'),
+    @Grab(group='com.google.cloud', module='google-cloud-core', version='1.65.0')]
+)
 import com.zebra.s2dl.jenkins.DataflowClient
 import com.zebra.s2dl.jenkins.JenkinsContext
 
 def call(Closure cl) {
   cl.resolveStrategy = Closure.DELEGATE_FIRST
-  cl.delegate = new DataflowClient(new JenkinsContext(this))
+  def ctx = new JenkinsContext(this)
+  cl.delegate = new DataflowClient(ctx)
   cl()
   return this
 }
+
