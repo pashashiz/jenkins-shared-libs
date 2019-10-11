ffdd07342f292a300961d9f98803b8df4f08ec26
pavlo <pavlo.pohrebnyi@asg.com>
2019-10-11 11:26:54 +0300

    Example of shared lib call (2)

--- vars/dataflow.groovy
+++ vars/dataflow.groovy
@@ -1,6 +1,6 @@
 import com.zebra.s2dl.jenkins.DataflowClient
 
-def call(Closure) {
+def call(Closure cl) {
   def code = cl.rehydrate(new DataflowClient(), this, this)
   code.resolveStrategy = Closure.DELEGATE_ONLY
   code()
